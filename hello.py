079831852a58676e0dd5c3b285580ab937d8b3b4
Erik <echu@cars.com>
2013-04-25 13:11:00 -0700

    added 98 things

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 
 
 # Wrote 3 Print Statements here
-print "howdy"
+print "Howdy"
 print "Her name is Esther"
-print "my favorite number is", 56
-
-
+print "Her favorite number is", 98
+#List all things that I have 98 of
+print I have 98 slices of pie in the fridge
+print I have seen 98 panda bears
+print I have 98 pairs of old shoes
