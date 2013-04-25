bdedd7321a1ec1a8f12539b06f84e7bfab1fd42b
Erik <echu@cars.com>
2013-04-25 13:19:03 -0700

    Created a variable for her favorite number

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-
+favorite_number = 76
 
 # Wrote 3 Print Statements here
 print "Howdy"
 print "Her name is Esther"
-print "Her favorite number is", 98
-#List all things that I have 98 of
-print I have 98 slices of pie in the fridge
-print I have seen 98 panda bears
-print I have 98 pairs of old shoes
+print "Her favorite number is", 106
+
+#List all things that I have 106 of
+print "I have 106 slices of pie in the fridge"
+print "I have seen 106 panda bears"
+print "I have 106 pairs of old shoes"
