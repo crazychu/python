180ca9958779194f05b5ead4a5f8729ed2760b0e
Erik <echu@cars.com>
2013-04-25 13:32:49 -0700

    we've added 2 variables, name & favorite number

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 favorite_number = 3
-
+person_name = "Erik"
 # Wrote 3 Print Statements here
 print "Howdy"
-print "Her name is Esther"
-print "Her favorite number is {}".format(favorite_number)
+print "My name is {}".format(person_name)
+print "My favorite number is {}".format(favorite_number)
 
 #List all things I have my favorite number of
-print "I have {} slices of pie in the fridge".format(favorite_number)
+print "{} has {} slices of pizza in the fridge".format(person_name,favorite_number)
 print "I have seen  {} panda bears".format(favorite_number)
 print "I have {} pairs of old shoes".format(favorite_number)
