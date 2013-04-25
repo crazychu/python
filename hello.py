28ff410cc4627ea6c4b7c544ad1ad900099174da
Erik <echu@cars.com>
2013-04-25 13:23:27 -0700

    changed my favorite number, comment

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-favorite_number = 76
+favorite_number = 3
 
 # Wrote 3 Print Statements here
 print "Howdy"
 print "Her name is Esther"
-print "Her favorite number is", 106
+print "Her favorite number is {}".format(favorite_number)
 
-#List all things that I have 106 of
-print "I have 106 slices of pie in the fridge"
-print "I have seen 106 panda bears"
-print "I have 106 pairs of old shoes"
+#List all things I have my favorite number of
+print "I have {} slices of pie in the fridge".format(favorite_number)
+print "I have seen  {} panda bears".format(favorite_number)
+print "I have {} pairs of old shoes".format(favorite_number)
