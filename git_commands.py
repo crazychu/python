9e00105ff545916f9168ea6b2bcb367c6f741db5
Erik <echu@cars.com>
2013-05-01 15:56:38 -0700

    Added git init & description

--- git_commands.py
+++ git_commands.py
@@ -8,3 +8,6 @@ print "git status"
 print "Shows which files are tracked or untracked, so you may commit, or, not commit."
 print " "
 #git log
+print "git log"
+print "The most basic and powerful tool to look back and see all the commits created."
+print " "
