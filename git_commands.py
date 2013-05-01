3aee3af73e9d76a388911c198dd017139667a2f2
Erik <echu@cars.com>
2013-05-01 15:55:50 -0700

    Added git init & description

--- git_commands.py
+++ git_commands.py
@@ -3,3 +3,8 @@ print " "
 print "git init"
 print "In the current directory, a git repository is created"
 print " "
+#git status
+print "git status"
+print "Shows which files are tracked or untracked, so you may commit, or, not commit."
+print " "
+#git log
