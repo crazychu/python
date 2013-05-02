3db6bbfd4a25b02a8bb36460a873689df02cff72
Erik <echu@cars.com>
2013-05-02 12:33:11 -0700

    Added Gender Variable

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
-favorite_number = 3
+favorite_number = 7
 person_name = "Erik"
+gender = "his"
+
 # Wrote 3 Print Statements here
 print "Howdy"
 print "My name is {}".format(person_name)
 print "My favorite number is {}".format(favorite_number)
 
 #List all things I have my favorite number of
-print "{} has {} slices of pizza in the fridge".format(person_name,favorite_number)
-print "I have seen  {} panda bears".format(favorite_number)
-print "I have {} pairs of old shoes".format(favorite_number)
+print "{} has {} slices of pizza in {} fridge".format(person_name,favorite_number,gender)
+print "{} has seen {} panda bears in {} backyard".format(person_name,favorite_number,gender)
+print "{} have {} pairs of old shoes in {} drawer".format(person_name,favorite_number,gender)
+
