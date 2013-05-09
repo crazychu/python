5a330ccddf0ebe41507d288e9b43bcdf80e32029
Erik <echu@cars.com>
2013-05-09 13:17:12 -0700

    added BOOLS

---
+++ names.py
@@ -0,0 +1,38 @@
+#teacher_names = ['andrew','bobby','esther']
+#hlallala
+
+#python_sentence = "{} loves Python! Whoo!".format(teacher_names[0])
+#print python_sentence
+#python_sentence = "{} loves Python! Whoo!".format(teacher_names[1])
+#print python_sentence
+#python_sentence = "{} loves Python! Whoo!".format(teacher_names[2])
+#print python_sentence
+
+
+#for teacher_name in teacher_names:
+#	python_sentence = "{} loves Python! Whoo!".format(teacher_name)
+#	print python_sentence
+	#*command executes:
+	#adnrew loves python!
+	#bobby love spythoN!
+	#esther love spython!
+#for iteration in range(1200):
+	#print "Loop {}:I love pizza".format(iteration)
+	#*this shoots out Loop 1188:I love pizza
+	#*this shoots out Loop 1189:I love pizza
+	#*this shoots out Loop 1190:I love pizza
+	#*this shoots out Loop 1191:I love pizza
+	#*this shoots out Loop 1192:I love pizza
+#iteration is a temporary variable 
+
+#print range(20)
+
+teacher_names = ['andrew','bobby','esther','travis']
+#print 'min' in teacher_names
+#spits out FALSE
+#print 'andrew' in teacher_names
+# >>> TRUE
+#print 'Andrew' in teacher_names
+# >>>FALSE > case-sensitive <
+# print 'andrew' not in teacher_names
+# >>> False, he IS in the list
