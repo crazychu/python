093e4998ab9437b00aa4f1bc64c3c5e9d1b7b016
Erik <echu@cars.com>
2013-05-16 13:29:13 -0700

    declaring functions, else functions

--- names.py
+++ names.py
@@ -24,10 +24,9 @@
 	#*this shoots out Loop 1191:I love pizza
 	#*this shoots out Loop 1192:I love pizza
 #iteration is a temporary variable 
-
 #print range(20)
 
-teacher_names = ['andrew','bobby','esther','travis']
+
 #print 'min' in teacher_names
 #spits out FALSE
 #print 'andrew' in teacher_names
@@ -36,3 +35,28 @@ teacher_names = ['andrew','bobby','esther','travis']
 # >>>FALSE > case-sensitive <
 # print 'andrew' not in teacher_names
 # >>> False, he IS in the list
+
+
+
+
+people = [('andrew','in'),
+				('bobby', 'in'),
+				('judy', 'out'),
+				('esther', 'in'),
+				('vince','in')]
+
+#declaring a function
+#upper is a method in a string that turns the text UPPER CASE
+def gets_reward():
+    print"{} is in the office.".format(person)
+    print"{} gets a bagel!".format(person)
+    print"{} gets to have so much cream cheese".format(person)
+#if the person is in the office, they get a bagel
+for person, status in people:
+    person = person.upper()
+    if status == 'in':
+        gets_reward()
+    else:
+        print"{} is not in the office.".format(person)
+        print "{} gets more emails...".format(person)
+        print "{} gets NO cream cheese.".format(person) 
