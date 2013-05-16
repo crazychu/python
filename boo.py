e6314e822b1c77fa94f7b612a7dc0c105aa78678
Erik <echu@cars.com>
2013-05-16 10:09:38 -0700

    MISSION COMPLETE: Homework Question 1

--- boo.py
+++ boo.py
@@ -1,12 +1,10 @@
-a = 6
-b = 7
-c = 42
-print 1, a == 6
-print 2, a == 7
-print 3, a == 6 and b == 7
-print 4, a == 7 and b == 7
-print 5, not a == 7 and b == 7
-print 6, a == 7 or b == 7
-print 7, a == 7 or b == 6
-print 8, not (a == 7 and b == 6)
-print 9, not a == 7 and b == 6
+#setting variables
+
+i = 1
+j = 5
+k = 3
+#printing, these will yeild TRUE or FALSE
+print a), i == k
+print b), j > k
+print c) j == 5
+print d) k < 1
