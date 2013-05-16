64d63e7dc80e1f5d83505facc2bd5833605626f4
Erik <echu@cars.com>
2013-05-16 10:11:48 -0700

    MISSION COMPLETE: Homework Question 1 (revised)

--- boo.py
+++ boo.py
@@ -4,7 +4,7 @@ i = 1
 j = 5
 k = 3
 #printing, these will yeild TRUE or FALSE
-print a), i == k
-print b), j > k
-print c) j == 5
-print d) k < 1
+print "a", i == "k"
+print "b", j > "k"
+print "c", j == 5
+print "d", k < 1
