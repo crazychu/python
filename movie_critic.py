442aca9bf5be113839b3a87e724ebd36a7994b21
Erik <echu@cars.com>
2013-05-23 10:18:27 -0700

    Movie critic: print statement

---
+++ movie_critic.py
@@ -0,0 +1,8 @@
+recent_movies = [('Iron Man 3', 78),
+                 ('The Great Gatsby', 48),
+                 ('Star Trek Into Darkness', 90),
+                 ('Oblivion', 56)]
+
+
+def gets_reaction():
+    print "I wanted {} the other day.".format(recent_movies)
